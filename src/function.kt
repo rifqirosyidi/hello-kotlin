d989995b81dc2b063a6547d56920d3f54eff878e
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-07-09 19:33:18 +0700

    function

--- src/function.kt
+++ src/function.kt
@@ -2,8 +2,13 @@ fun sayHello(name: String) {
     println("Hello $name")
 }
 
+// Single line Ver
+// fun sayHello(name: String) = println("Hello $name")
+
+fun sayHi(greet: String, name: String) = println("$greet $name")
 
 fun main() {
     sayHello("Rifqi")
     sayHello( "Universe")
+    sayHi("Welcome", "Rifqi")
 }
